7f7faba4736c3d7a0bf65d4e7200a291c3dfe4a0
Mustafa GUNES <gunes149@gmail.com>
2024-02-12 19:38:39 +0300

    update spm

--- Package.swift
+++ Package.swift
@@ -1,3 +1,6 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -5,13 +8,15 @@ let package = Package(
     products: [
         .library(
             name: "SwiftyVK",
-            targets: ["SwiftyVK_macOS", "SwiftyVK_iOS"]
+            targets: ["SwiftyVK"]
         ),
-        targets: [
-        .target(name: "SwiftyVK_macOS", dependencies: ["SwiftyVK_resources_macOS"]),
-        .target(name: "SwiftyVK_iOS", dependencies: ["SwiftyVK_resources_iOS"]),
-        .testTarget(name: "SwiftyVK_tests_macOS", dependencies: ["SwiftyVK_macOS"]),
-        .testTarget(name: "SwiftyVK_tests_iOS", dependencies: ["SwiftyVK_iOS"]),
-        ]
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SwiftyVK",
+            dependencies: [],
+            path: "Library/Sources"
+        )
     ]
 )
